429c2df895521a173a1d98fe49b17798d9d7a57f
Raushan710 <69067852+Raushan710@users.noreply.github.com>
2020-08-18 14:36:44 +0530

    Add files via upload

---
+++ leaders.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  int A[n];
+  for(int i=0;i<n;i++)
+  cin>>A[i];
+  
+for(int i=0;i<n;i++) 
+    { 
+        int j; 
+        for(j=i+1;j<n;j++) 
+        { 
+            if(A[i]<A[j]) 
+                break; 
+        }     
+        if(j==n)
+          cout<<A[i]<<" "; 
+    }
+  return 0;
+}
